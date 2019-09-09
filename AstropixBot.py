af63ab5d9d26aef7e758ebbbb5f5105cb2d2edcb
Frax7 <10byou.aou@gmail.com>
2019-09-09 13:29:52 +0200

    First commit

---
+++ AstropixBot.py
@@ -0,0 +1,71 @@
+
+import json
+import telegram
+from datetime import date
+from urllib.request import urlopen
+from youtubeID import get_yt_video_id
+
+# Create the Bot object
+token = "TOKEN"
+chat_id = "CHAT_ID"
+bot = telegram.Bot(token)
+
+# Pick today's page and today's link for JSON.
+api_key = "API_KEY"
+today = date.today()
+today_link = 'http://apod.nasa.gov/apod/ap'+today.strftime("%y%m%d")+'.html'
+link = "https://api.nasa.gov/planetary/apod?api_key="+api_key+"&date="+today.strftime("%Y-%m-%d")
+
+with urlopen(link) as response:
+    # .decode("utf-8") is for converting bytes into string.
+    source = response.read().decode("utf-8")
+    data = json.loads(source)
+
+# Picking all relevant information from the JSON    .
+media_type = data["media_type"]
+url = data["url"]
+title = data["title"]
+
+# Diferenciate between image and video content
+if (media_type == "image"):
+    # In case of image, will download the image and then send
+    # it with a caption(title + today's link).
+
+    # Start downloading the image.
+    with open("image.jpeg",'wb') as imagefile:
+        imagefile.write(urlopen(url).read())
+    # Image downloaded.
+
+    # Open and send the image with its caption.
+    with open('image.jpeg', 'rb') as image:
+        bot.send_photo(
+            chat_id,
+            image,
+            caption = "<b>"+title+"</b>\n <a href='"+today_link+"'>Check the explanation</a>.",
+            parse_mode='html')
+    # Image sended.
+    print("Image sended !")
+    
+
+elif (media_type == "video"):
+    # In case of video, will download the thumbnail and then send
+    # it with a caption ([VIDEO]title + video's link + today's link)
+
+    # Use the script to get the youtube video ID.
+    yt_video_id = get_yt_video_id(url)
+    yt_thumbnail_link = "https://img.youtube.com/vi/"+yt_video_id+"/hqdefault.jpg"
+    
+    with open("image.jpg",'wb') as imagefile:
+        imagefile.write(urlopen(yt_thumbnail_link).read())
+    # Thumbnail downloaded.
+    
+    # Open and send the thumbnail with its caption.
+    with open('image.jpg', 'rb') as image:
+        bot.send_photo(
+            chat_id,
+            image,
+            caption = "[VIDEO] <b>"+title+"</b>\n <a href='"+url+"'>Watch full video.</a>\n <a href='"+today_link+"'>Check the explanation.</a>",
+            parse_mode='html')
+    # Thumbnail sended.
+    print("Thumbnail sended !")
+
